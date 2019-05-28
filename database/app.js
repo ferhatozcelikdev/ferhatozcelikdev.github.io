cfa9c5db4191cda83a8c03b14f3e9a93bc87f34f
Ferhat ÖZÇELİK <50930434+ferhatozcelikdev@users.noreply.github.com>
2019-05-29 00:57:39 +0300

    Add files via upload

---
+++ database/app.js
@@ -0,0 +1,14 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBSs7VAHeCmFjIil-9D-6qdKKVI85YQUfE",
+    authDomain: "linnerregrasyon.firebaseapp.com",
+    databaseURL: "https://linnerregrasyon.firebaseio.com",
+    projectId: "linnerregrasyon",
+    storageBucket: "linnerregrasyon.appspot.com",
+    messagingSenderId: "161829971746",
+    appId: "1:161829971746:web:de743c7825ce61a1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
